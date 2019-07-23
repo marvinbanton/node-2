8dc2f24114becc6880a7648dda60a9796cd2a957
marvin banton <marvin.banton@boom.camp>
2019-07-23 16:00:54 +0800

    Finished step 4, can create user profile

--- server/controllers/manageUser.js
+++ server/controllers/manageUser.js
@@ -6,9 +6,26 @@ module.exports = {
             id: db.users.id,
             email,
             password,
+        };
+        const profiles = {
+            userId: db.profiles.id,
+            thumbnail: '',
+            about: '',
         }
+
         db.users.data.push(users);
-        res.status(200).send(users);
+        db.profiles.data.push(profiles);
+        Object.assign(db, {
+            users: {
+                id: db.users.id + 1,
+                data: db.users.data
+            },
+            profiles: {
+                id: db.profiles.id + 1,
+                data: db.profiles.data
+            }
+        })
+        res.status(200).send(db);
     }
 
 
